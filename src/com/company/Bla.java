0326aeea1423c2a033f21b064b4245bfaa5c96eb
Federico <pfahlf@usi.ch>
2019-10-27 14:45:49 +0100

    Fix issue #1

--- src/com/company/Bla.java
+++ src/com/company/Bla.java
@@ -5,5 +5,6 @@ public class Bla {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Mom");
+        System.out.println("HEY!!");
     }
 }
