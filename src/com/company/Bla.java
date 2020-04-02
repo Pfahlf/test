64f98ca9bf2c386ff36cda1654168200fc1b87c1
Federico Pfahler <federico.pfahler@usi.ch>
2020-04-02 13:13:31 +0200

    Reduce loc

--- src/com/company/Bla.java
+++ src/com/company/Bla.java
@@ -3,8 +3,6 @@ package com.company;
 public class Bla {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello Mom");
-        System.out.println("HEY!!");
+
     }
 }
